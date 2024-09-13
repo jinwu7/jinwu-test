a133ae3890f62451ed0a9fa336773659ccd4302d
jinwu7 <15629134358@163.com>
2024-09-13 10:33:25 +0800

    commit

---
+++ ruoyi/ruoyi-common/src/main/java/com/ruoyi/common/annotation/Anonymous.java
@@ -0,0 +1,18 @@
+package com.ruoyi.common.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 匿名访问不鉴权注解
+ * 
+ * @author ruoyi
+ */
+@Target({ Elemen.RUNTIME)
+@Documented
+public @interface Anonymous
+{
+}
